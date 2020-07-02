f7e60fab09741c84f663de7d4ab6d9a48644e690
Aman Mahendroo <30946991+amanMahendroo@users.noreply.github.com>
2020-07-02 09:57:30 +0530

    update
    Runs in background

--- content.js
+++ content.js
@@ -1,17 +1,18 @@
-let t0 = t1 = 0
+let t0 = new Date()
 let attention = 20
 let pause = 5
 let cycle = attention + pause
 
-t0 = new Date()
-setInterval(() => {
-	t1 = new Date()
-	if (t1 - t0 > attention * 60 * 1000 && t1 - t0 - 1000 < attention * 60 * 1000) {
+setInterval(timer, 1000)
+
+function timer() {
+	dt = new Date() - t0
+	if (dt > attention * 60 * 1000 && dt - 1000 < attention * 60 * 1000) {
 		alert("time to take a break!")
 	}
 
-	if (t1 - t0 > cycle * 60 * 1000 && t1 - t0 - 1000 < cycle * 60 * 1000) {
+	if (dt > cycle * 60 * 1000 && dt - 1000 < cycle * 60 * 1000) {
 		alert("let's get back to work!")
 		t0 = new Date()
 	}
-}, 1000)
+}
